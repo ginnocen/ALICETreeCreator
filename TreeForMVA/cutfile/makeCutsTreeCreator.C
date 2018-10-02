b091ce4486d39d846c90afdd77203b515dc94ed9
Andrea Festanti <andrea.festanti@cern.ch>
2018-10-02 15:55:16 +0200

    adding README

--- TreeForMVA/cutfile/makeCutsTreeCreator.C
+++ TreeForMVA/cutfile/makeCutsTreeCreator.C
@@ -10,28 +10,31 @@
 #include "makeInputCutsDstoKKpi.C"
 #include "makeInputCutsDplustoKpipi.C"
 
+Float_ minCent=0.;
+Float_ maxCent=100.;
+
 
 void makeCutsTreeCreator()
 {
     Printf("D0 filtering cuts");
-    AliRDHFCutsD0toKpi  *looseCutsD0toKpi    = makeInputCutsD0toKpi(0,"D0toKpiFilteringCuts",0.,100.);
+    AliRDHFCutsD0toKpi  *looseCutsD0toKpi    = makeInputCutsD0toKpi(0,"D0toKpiFilteringCuts",minCent,maxCent);
     Printf("\n\n");
     Printf("D0 analysis cuts");
-    AliRDHFCutsD0toKpi  *analysisCutsD0toKpi = makeInputCutsD0toKpi(1,"D0toKpiAnalysisCuts",0.,100.);
+    AliRDHFCutsD0toKpi  *analysisCutsD0toKpi = makeInputCutsD0toKpi(1,"D0toKpiAnalysisCuts",minCent,maxCent);
     Printf("\n\n");
     Printf("*************************************************************");
     Printf("Ds filtering cuts");
-    AliRDHFCutsDstoKKpi  *looseCutsDstoKKpi    = makeInputCutsDstoKKpi(0,"DstoKKpiFilteringCuts",0.,100.);
+    AliRDHFCutsDstoKKpi  *looseCutsDstoKKpi    = makeInputCutsDstoKKpi(0,"DstoKKpiFilteringCuts",minCent,maxCent);
     Printf("\n\n");
     Printf("Ds analysis cuts");
-    AliRDHFCutsDstoKKpi  *analysisCutsDstoKKpi = makeInputCutsDstoKKpi(1,"DstoKKpiAnalysisCuts",0.,100.);
+    AliRDHFCutsDstoKKpi  *analysisCutsDstoKKpi = makeInputCutsDstoKKpi(1,"DstoKKpiAnalysisCuts",minCent,maxCent);
     Printf("\n\n");
     Printf("*************************************************************");
     Printf("Dplus filtering cuts");
-    AliRDHFCutsDplustoKpipi  *looseCutsDplustoKpipi    = makeInputCutsDplustoKpipi(0,"DplustoKpipiFilteringCuts",0.,100.);
+    AliRDHFCutsDplustoKpipi  *looseCutsDplustoKpipi    = makeInputCutsDplustoKpipi(0,"DplustoKpipiFilteringCuts",minCent,maxCent);
     Printf("\n\n");
     Printf("Dplus analysis cuts");
-    AliRDHFCutsDplustoKpipi  *analysisCutsDplustoKpipi = makeInputCutsDplustoKpipi(1,"DplustoKpipiAnalysisCuts",0.,100.);
+    AliRDHFCutsDplustoKpipi  *analysisCutsDplustoKpipi = makeInputCutsDplustoKpipi(1,"DplustoKpipiAnalysisCuts",minCent,maxCent);
     
     TFile* fout=new TFile("D0DsDplusCuts.root","recreate");
     fout->cd();
