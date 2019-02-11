9a59a1d052b11a455f719f0dba2d36d54120bab5
Luuk Vermunt <luuk.vermunt@cern.ch>
2019-02-11 11:17:48 +0000

    Bug fixes for empty branches

--- ProcessOutputTTreeMVA/includeSkim/skimTreeLctopK0sFromEvt.C
+++ ProcessOutputTTreeMVA/includeSkim/skimTreeLctopK0sFromEvt.C
@@ -10,7 +10,7 @@ using namespace std;
 
 //Commented sections is code for the additional information in the big TTree that might need to be saved to the skimmed ttree at a later point.
 
-void skimTreeLcFromEvt(TString input="AnalysisResults.root",TString output="test.root",TString ttreeout="tree_LctopK0s", Bool_t isMC = kFALSE, Bool_t ispp = kFALSE){
+void skimTreeLcFromEvt(TString input="AnalysisResults.root",TString output="test.root",TString ttreeout="tree_LctopK0s", Bool_t isMC = kFALSE, Bool_t ispp = kFALSE, Bool_t wasLcSecVertexReconstructed = kFALSE){
 
   TFile *f = TFile::Open(input.Data());
   TDirectory * dir = (TDirectory*)f->Get("PWGHF_TreeCreator");
@@ -197,98 +197,108 @@ void skimTreeLcFromEvt(TString input="AnalysisResults.root",TString output="test
     if(is_ev_rej_ML!=0) continue;
       
     for(int icand = 0; icand < t.n_cand; icand++){
-      if(t.inv_mass) inv_mass_ML=t.inv_mass -> at(icand);
-      if(t.pt_cand) pt_cand_ML=t.pt_cand -> at(icand);
-      if(t.d_len) d_len_ML=t.d_len -> at(icand);
-      if(t.d_len_xy) d_len_xy_ML=t.d_len_xy -> at(icand);
-      if(t.norm_dl_xy) norm_dl_xy_ML=t.norm_dl_xy -> at(icand);
-      if(t.cos_p) cos_p_ML=t.cos_p -> at(icand);
-      if(t.cos_p_xy) cos_p_xy_ML=t.cos_p_xy -> at(icand);
-      if(t.imp_par_xy) imp_par_xy_ML=t.imp_par_xy -> at(icand);
-      if(t.dca) dca_ML=t.dca -> at(icand);
-      if(t.cos_t_star) cos_t_star_ML=t.cos_t_star -> at(icand);
-      if(t.signd0) signd0_ML=t.signd0 -> at(icand);
-      if(t.inv_mass_K0s) inv_mass_K0s_ML=t.inv_mass_K0s -> at(icand);
-      if(t.dca_K0s) dca_K0s_ML=t.dca_K0s -> at(icand);
-      if(t.imp_par_K0s) imp_par_K0s_ML=t.imp_par_K0s -> at(icand);
-      if(t.d_len_K0s) d_len_K0s_ML=t.d_len_K0s -> at(icand);
-      if(t.armenteros_K0s) armenteros_K0s_ML=t.armenteros_K0s -> at(icand);
-      if(t.ctau_K0s) ctau_K0s_ML=t.ctau_K0s -> at(icand);
-      if(t.cos_p_K0s) cos_p_K0s_ML=t.cos_p_K0s -> at(icand);
-      if(t.pt_K0s) pt_K0s_ML=t.pt_K0s -> at(icand);
-      if(t.eta_K0s) eta_K0s_ML=t.eta_K0s -> at(icand);
-      if(t.phi_K0s) phi_K0s_ML=t.phi_K0s -> at(icand);
+      if(t.inv_mass) inv_mass_ML=t.inv_mass -> at(icand); else inv_mass_ML=0;
+      if(t.pt_cand) pt_cand_ML=t.pt_cand -> at(icand);    else pt_cand_ML=0;
+      if(wasLcSecVertexReconstructed){
+        if(t.d_len) d_len_ML=t.d_len -> at(icand);
+        if(t.d_len_xy) d_len_xy_ML=t.d_len_xy -> at(icand);
+        if(t.norm_dl_xy) norm_dl_xy_ML=t.norm_dl_xy -> at(icand);
+        if(t.cos_p) cos_p_ML=t.cos_p -> at(icand);
+        if(t.cos_p_xy) cos_p_xy_ML=t.cos_p_xy -> at(icand);
+        if(t.imp_par_xy) imp_par_xy_ML=t.imp_par_xy -> at(icand);
+        if(t.dca) dca_ML=t.dca -> at(icand);
+      } else {
+        d_len_ML=0;
+        d_len_xy_ML=0;
+        norm_dl_xy_ML=0;
+        cos_p_ML=0;
+        cos_p_xy_ML=0;
+        imp_par_xy_ML=0;
+        dca_ML=0;
+      }
+      if(t.cos_t_star) cos_t_star_ML=t.cos_t_star -> at(icand);             else cos_t_star_ML=0;
+      if(t.signd0) signd0_ML=t.signd0 -> at(icand);                         else signd0_ML=0;
+      if(t.inv_mass_K0s) inv_mass_K0s_ML=t.inv_mass_K0s -> at(icand);       else inv_mass_K0s_ML=0;
+      if(t.dca_K0s) dca_K0s_ML=t.dca_K0s -> at(icand);                      else dca_K0s_ML=0;
+      if(t.imp_par_K0s) imp_par_K0s_ML=t.imp_par_K0s -> at(icand);          else imp_par_K0s_ML=0;
+      if(t.d_len_K0s) d_len_K0s_ML=t.d_len_K0s -> at(icand);                else d_len_K0s_ML=0;
+      if(t.armenteros_K0s) armenteros_K0s_ML=t.armenteros_K0s -> at(icand); else armenteros_K0s_ML=0;
+      if(t.ctau_K0s) ctau_K0s_ML=t.ctau_K0s -> at(icand);                   else ctau_K0s_ML=0;
+      if(t.cos_p_K0s) cos_p_K0s_ML=t.cos_p_K0s -> at(icand);                else cos_p_K0s_ML=0;
+      if(t.pt_K0s) pt_K0s_ML=t.pt_K0s -> at(icand);                         else pt_K0s_ML=0;
+      if(t.eta_K0s) eta_K0s_ML=t.eta_K0s -> at(icand);                      else eta_K0s_ML=0;
+      if(t.phi_K0s) phi_K0s_ML=t.phi_K0s -> at(icand);                      else phi_K0s_ML=0;
       
-      if(t.cand_type) cand_type_ML=t.cand_type -> at(icand);
-      if(t.y_cand) y_cand_ML=t.y_cand -> at(icand);
-      if(t.eta_cand) eta_cand_ML=t.eta_cand -> at(icand);
-      if(t.phi_cand) phi_cand_ML=t.phi_cand -> at(icand);
+      if(t.cand_type) cand_type_ML=t.cand_type -> at(icand); else cand_type_ML=0;
+      if(t.y_cand) y_cand_ML=t.y_cand -> at(icand);          else y_cand_ML=0;
+      if(t.eta_cand) eta_cand_ML=t.eta_cand -> at(icand);    else eta_cand_ML=0;
+      if(t.phi_cand) phi_cand_ML=t.phi_cand -> at(icand);    else phi_cand_ML=0;
 
-      if(t.pt_prong0) pt_prong0_ML=t.pt_prong0 -> at(icand);
-      if(t.pt_prong1) pt_prong1_ML=t.pt_prong1 -> at(icand);
-      if(t.pt_prong2) pt_prong2_ML=t.pt_prong2 -> at(icand);
-      if(t.imp_par_prong0) imp_par_prong0_ML=t.imp_par_prong0 -> at(icand);
-      if(t.imp_par_prong1) imp_par_prong1_ML=t.imp_par_prong1 -> at(icand);
-      if(t.imp_par_prong2) imp_par_prong2_ML=t.imp_par_prong2 -> at(icand);
-      if(t.p_prong0) p_prong0_ML=t.p_prong0 -> at(icand);
-      if(t.p_prong1) p_prong1_ML=t.p_prong1 -> at(icand);
-      if(t.p_prong2) p_prong2_ML=t.p_prong2 -> at(icand);
-      if(t.eta_prong0) eta_prong0_ML=t.eta_prong0 -> at(icand);
-      if(t.eta_prong1) eta_prong1_ML=t.eta_prong1 -> at(icand);
-      if(t.eta_prong2) eta_prong2_ML=t.eta_prong2 -> at(icand);
-      if(t.phi_prong0) phi_prong0_ML=t.phi_prong0 -> at(icand);
-      if(t.phi_prong1) phi_prong1_ML=t.phi_prong1 -> at(icand);
-      if(t.phi_prong2) phi_prong2_ML=t.phi_prong2 -> at(icand);
+      if(t.pt_prong0) pt_prong0_ML=t.pt_prong0 -> at(icand);                else pt_prong0_ML=0;
+      if(t.pt_prong1) pt_prong1_ML=t.pt_prong1 -> at(icand);                else pt_prong1_ML=0;
+      if(t.pt_prong2) pt_prong2_ML=t.pt_prong2 -> at(icand);                else pt_prong2_ML=0;
+      if(t.imp_par_prong0) imp_par_prong0_ML=t.imp_par_prong0 -> at(icand); else imp_par_prong0_ML=0;
+      if(t.imp_par_prong1) imp_par_prong1_ML=t.imp_par_prong1 -> at(icand); else imp_par_prong1_ML=0;
+      if(t.imp_par_prong2) imp_par_prong2_ML=t.imp_par_prong2 -> at(icand); else imp_par_prong2_ML=0;
+      if(t.p_prong0) p_prong0_ML=t.p_prong0 -> at(icand);                   else p_prong0_ML=0;
+      if(t.p_prong1) p_prong1_ML=t.p_prong1 -> at(icand);                   else p_prong1_ML=0;
+      if(t.p_prong2) p_prong2_ML=t.p_prong2 -> at(icand);                   else p_prong2_ML=0;
+      if(t.eta_prong0) eta_prong0_ML=t.eta_prong0 -> at(icand);             else eta_prong0_ML=0;
+      if(t.eta_prong1) eta_prong1_ML=t.eta_prong1 -> at(icand);             else eta_prong1_ML=0;
+      if(t.eta_prong2) eta_prong2_ML=t.eta_prong2 -> at(icand);             else eta_prong2_ML=0;
+      if(t.phi_prong0) phi_prong0_ML=t.phi_prong0 -> at(icand);             else phi_prong0_ML=0;
+      if(t.phi_prong1) phi_prong1_ML=t.phi_prong1 -> at(icand);             else phi_prong1_ML=0;
+      if(t.phi_prong2) phi_prong2_ML=t.phi_prong2 -> at(icand);             else phi_prong2_ML=0;
 
-      if(t.nTPCcls_prong0) nTPCcls_prong0_ML=t.nTPCcls_prong0 -> at(icand);
-      if(t.nTPCclspid_prong0) nTPCclspid_prong0_ML=t.nTPCclspid_prong0 -> at(icand);
-      if(t.nTPCcrossrow_prong0) nTPCcrossrow_prong0_ML=t.nTPCcrossrow_prong0 -> at(icand);
-      if(t.chi2perndf_prong0) chi2perndf_prong0_ML=t.chi2perndf_prong0 -> at(icand);
-      if(t.nITScls_prong0) nITScls_prong0_ML=t.nITScls_prong0 -> at(icand);
-      if(t.ITSclsmap_prong0) ITSclsmap_prong0_ML=t.ITSclsmap_prong0 -> at(icand);
-      if(t.nTPCcls_prong1) nTPCcls_prong1_ML=t.nTPCcls_prong1 -> at(icand);
-      if(t.nTPCclspid_prong1) nTPCclspid_prong1_ML=t.nTPCclspid_prong1 -> at(icand);
-      if(t.nTPCcrossrow_prong1) nTPCcrossrow_prong1_ML=t.nTPCcrossrow_prong1 -> at(icand);
-      if(t.chi2perndf_prong1) chi2perndf_prong1_ML=t.chi2perndf_prong1 -> at(icand);
-      if(t.nITScls_prong1) nITScls_prong1_ML=t.nITScls_prong1 -> at(icand);
-      if(t.ITSclsmap_prong1) ITSclsmap_prong1_ML=t.ITSclsmap_prong1 -> at(icand);
-      if(t.nTPCcls_prong2) nTPCcls_prong2_ML=t.nTPCcls_prong2 -> at(icand);
-      if(t.nTPCclspid_prong2) nTPCclspid_prong2_ML=t.nTPCclspid_prong2 -> at(icand);
-      if(t.nTPCcrossrow_prong2) nTPCcrossrow_prong2_ML=t.nTPCcrossrow_prong2 -> at(icand);
-      if(t.chi2perndf_prong2) chi2perndf_prong2_ML=t.chi2perndf_prong2 -> at(icand);
-      if(t.nITScls_prong2) nITScls_prong2_ML=t.nITScls_prong2 -> at(icand);
-      if(t.ITSclsmap_prong2) ITSclsmap_prong2_ML=t.ITSclsmap_prong2 -> at(icand);
+      if(t.nTPCcls_prong0) nTPCcls_prong0_ML=t.nTPCcls_prong0 -> at(icand);                else nTPCcls_prong0_ML=0;
+      if(t.nTPCclspid_prong0) nTPCclspid_prong0_ML=t.nTPCclspid_prong0 -> at(icand);       else nTPCclspid_prong0_ML=0;
+      if(t.nTPCcrossrow_prong0) nTPCcrossrow_prong0_ML=t.nTPCcrossrow_prong0 -> at(icand); else nTPCcrossrow_prong0_ML=0;
+      if(t.chi2perndf_prong0) chi2perndf_prong0_ML=t.chi2perndf_prong0 -> at(icand);       else chi2perndf_prong0_ML=0;
+      if(t.nITScls_prong0) nITScls_prong0_ML=t.nITScls_prong0 -> at(icand);                else nITScls_prong0_ML=0;
+      if(t.ITSclsmap_prong0) ITSclsmap_prong0_ML=t.ITSclsmap_prong0 -> at(icand);          else ITSclsmap_prong0_ML=0;
+      if(t.nTPCcls_prong1) nTPCcls_prong1_ML=t.nTPCcls_prong1 -> at(icand);                else nTPCcls_prong1_ML=0;
+      if(t.nTPCclspid_prong1) nTPCclspid_prong1_ML=t.nTPCclspid_prong1 -> at(icand);       else nTPCclspid_prong1_ML=0;
+      if(t.nTPCcrossrow_prong1) nTPCcrossrow_prong1_ML=t.nTPCcrossrow_prong1 -> at(icand); else nTPCcrossrow_prong1_ML=0;
+      if(t.chi2perndf_prong1) chi2perndf_prong1_ML=t.chi2perndf_prong1 -> at(icand);       else chi2perndf_prong1_ML=0;
+      if(t.nITScls_prong1) nITScls_prong1_ML=t.nITScls_prong1 -> at(icand);                else nITScls_prong1_ML=0;
+      if(t.ITSclsmap_prong1) ITSclsmap_prong1_ML=t.ITSclsmap_prong1 -> at(icand);          else ITSclsmap_prong1_ML=0;
+      if(t.nTPCcls_prong2) nTPCcls_prong2_ML=t.nTPCcls_prong2 -> at(icand);                else ITSclsmap_prong1_ML=0;
+      if(t.nTPCclspid_prong2) nTPCclspid_prong2_ML=t.nTPCclspid_prong2 -> at(icand);       else nTPCclspid_prong2_ML=0;
+      if(t.nTPCcrossrow_prong2) nTPCcrossrow_prong2_ML=t.nTPCcrossrow_prong2 -> at(icand); else nTPCcrossrow_prong2_ML=0;
+      if(t.chi2perndf_prong2) chi2perndf_prong2_ML=t.chi2perndf_prong2 -> at(icand);       else chi2perndf_prong2_ML=0;
+      if(t.nITScls_prong2) nITScls_prong2_ML=t.nITScls_prong2 -> at(icand);                else nITScls_prong2_ML=0;
+      if(t.ITSclsmap_prong2) ITSclsmap_prong2_ML=t.ITSclsmap_prong2 -> at(icand);          else ITSclsmap_prong2_ML=0;
 
-      if(t.nsigTPC_Pi_0) nsigTPC_Pi_0_ML=t.nsigTPC_Pi_0 -> at(icand);
-      if(t.nsigTPC_Pr_0) nsigTPC_Pr_0_ML=t.nsigTPC_Pr_0 -> at(icand);
-      if(t.nsigTOF_Pi_0) nsigTOF_Pi_0_ML=t.nsigTOF_Pi_0 -> at(icand);
-      if(t.nsigTOF_Pr_0) nsigTOF_Pr_0_ML=t.nsigTOF_Pr_0 -> at(icand);
-      if(t.dEdxTPC_0) dEdxTPC_0_ML=t.dEdxTPC_0 -> at(icand);
-      if(t.ToF_0) ToF_0_ML=t.ToF_0 -> at(icand);
-      if(t.pTPC_prong0) pTPC_prong0_ML=t.pTPC_prong0 -> at(icand);
-      if(t.pTOF_prong0) pTOF_prong0_ML=t.pTOF_prong0 -> at(icand);
-      if(t.trlen_prong0) trlen_prong0_ML=t.trlen_prong0 -> at(icand);
-      if(t.start_time_res_prong0) start_time_res_prong0_ML=t.start_time_res_prong0 -> at(icand);
-      if(t.nsigTPC_Pi_1) nsigTPC_Pi_1_ML=t.nsigTPC_Pi_1 -> at(icand);
-      if(t.nsigTPC_Pr_1) nsigTPC_Pr_1_ML=t.nsigTPC_Pr_1 -> at(icand);
-      if(t.nsigTOF_Pi_1) nsigTOF_Pi_1_ML=t.nsigTOF_Pi_1 -> at(icand);
-      if(t.nsigTOF_Pr_1) nsigTOF_Pr_1_ML=t.nsigTOF_Pr_1 -> at(icand);
-      if(t.dEdxTPC_1) dEdxTPC_1_ML=t.dEdxTPC_1 -> at(icand);
-      if(t.ToF_1) ToF_1_ML=t.ToF_1 -> at(icand);
-      if(t.pTPC_prong1) pTPC_prong1_ML=t.pTPC_prong1 -> at(icand);
-      if(t.pTOF_prong1) pTOF_prong1_ML=t.pTOF_prong1 -> at(icand);
-      if(t.trlen_prong1) trlen_prong1_ML=t.trlen_prong1 -> at(icand);
-      if(t.start_time_res_prong1) start_time_res_prong1_ML=t.start_time_res_prong1 -> at(icand);
-      if(t.nsigTPC_Pi_2) nsigTPC_Pi_2_ML=t.nsigTPC_Pi_2 -> at(icand);
-      if(t.nsigTPC_Pr_2) nsigTPC_Pr_2_ML=t.nsigTPC_Pr_2 -> at(icand);
-      if(t.nsigTOF_Pi_2) nsigTOF_Pi_2_ML=t.nsigTOF_Pi_2 -> at(icand);
-      if(t.nsigTOF_Pr_2) nsigTOF_Pr_2_ML=t.nsigTOF_Pr_2 -> at(icand);
-      if(t.dEdxTPC_2) dEdxTPC_2_ML=t.dEdxTPC_2 -> at(icand);
-      if(t.ToF_2) ToF_2_ML=t.ToF_2 -> at(icand);
-      if(t.pTPC_prong2) pTPC_prong2_ML=t.pTPC_prong2 -> at(icand);
-      if(t.pTOF_prong2) pTOF_prong2_ML=t.pTOF_prong2 -> at(icand);
-      if(t.trlen_prong2) trlen_prong2_ML=t.trlen_prong2 -> at(icand);
-      if(t.start_time_res_prong2) start_time_res_prong2_ML=t.start_time_res_prong2 -> at(icand);
+      if(t.nsigTPC_Pi_0) nsigTPC_Pi_0_ML=t.nsigTPC_Pi_0 -> at(icand);                            else nsigTPC_Pi_0_ML=0;
+      if(t.nsigTPC_Pr_0) nsigTPC_Pr_0_ML=t.nsigTPC_Pr_0 -> at(icand);                            else nsigTPC_Pr_0_ML=0;
+      if(t.nsigTOF_Pi_0) nsigTOF_Pi_0_ML=t.nsigTOF_Pi_0 -> at(icand);                            else nsigTOF_Pi_0_ML=0;
+      if(t.nsigTOF_Pr_0) nsigTOF_Pr_0_ML=t.nsigTOF_Pr_0 -> at(icand);                            else nsigTOF_Pr_0_ML=0;
+      if(t.dEdxTPC_0) dEdxTPC_0_ML=t.dEdxTPC_0 -> at(icand);                                     else dEdxTPC_0_ML=0;
+      if(t.ToF_0) ToF_0_ML=t.ToF_0 -> at(icand);                                                 else ToF_0_ML=0;
+      if(t.pTPC_prong0) pTPC_prong0_ML=t.pTPC_prong0 -> at(icand);                               else pTPC_prong0_ML=0;
+      if(t.pTOF_prong0) pTOF_prong0_ML=t.pTOF_prong0 -> at(icand);                               else pTOF_prong0_ML=0;
+      if(t.trlen_prong0) trlen_prong0_ML=t.trlen_prong0 -> at(icand);                            else trlen_prong0_ML=0;
+      if(t.start_time_res_prong0) start_time_res_prong0_ML=t.start_time_res_prong0 -> at(icand); else start_time_res_prong0_ML=0;
+      if(t.nsigTPC_Pi_1) nsigTPC_Pi_1_ML=t.nsigTPC_Pi_1 -> at(icand);                            else nsigTPC_Pi_1_ML=0;
+      if(t.nsigTPC_Pr_1) nsigTPC_Pr_1_ML=t.nsigTPC_Pr_1 -> at(icand);                            else nsigTPC_Pr_1_ML=0;
+      if(t.nsigTOF_Pi_1) nsigTOF_Pi_1_ML=t.nsigTOF_Pi_1 -> at(icand);                            else nsigTOF_Pi_1_ML=0;
+      if(t.nsigTOF_Pr_1) nsigTOF_Pr_1_ML=t.nsigTOF_Pr_1 -> at(icand);                            else nsigTOF_Pr_1_ML=0;
+      if(t.dEdxTPC_1) dEdxTPC_1_ML=t.dEdxTPC_1 -> at(icand);                                     else dEdxTPC_1_ML=0;
+      if(t.ToF_1) ToF_1_ML=t.ToF_1 -> at(icand);                                                 else ToF_1_ML=0;
+      if(t.pTPC_prong1) pTPC_prong1_ML=t.pTPC_prong1 -> at(icand);                               else pTPC_prong1_ML=0;
+      if(t.pTOF_prong1) pTOF_prong1_ML=t.pTOF_prong1 -> at(icand);                               else pTOF_prong1_ML=0;
+      if(t.trlen_prong1) trlen_prong1_ML=t.trlen_prong1 -> at(icand);                            else trlen_prong1_ML=0;
+      if(t.start_time_res_prong1) start_time_res_prong1_ML=t.start_time_res_prong1 -> at(icand); else start_time_res_prong1_ML=0;
+      if(t.nsigTPC_Pi_2) nsigTPC_Pi_2_ML=t.nsigTPC_Pi_2 -> at(icand);                            else nsigTPC_Pi_2_ML=0;
+      if(t.nsigTPC_Pr_2) nsigTPC_Pr_2_ML=t.nsigTPC_Pr_2 -> at(icand);                            else nsigTPC_Pr_2_ML=0;
+      if(t.nsigTOF_Pi_2) nsigTOF_Pi_2_ML=t.nsigTOF_Pi_2 -> at(icand);                            else nsigTOF_Pi_2_ML=0;
+      if(t.nsigTOF_Pr_2) nsigTOF_Pr_2_ML=t.nsigTOF_Pr_2 -> at(icand);                            else nsigTOF_Pr_2_ML=0;
+      if(t.dEdxTPC_2) dEdxTPC_2_ML=t.dEdxTPC_2 -> at(icand);                                     else dEdxTPC_2_ML=0;
+      if(t.ToF_2) ToF_2_ML=t.ToF_2 -> at(icand);                                                 else ToF_2_ML=0;
+      if(t.pTPC_prong2) pTPC_prong2_ML=t.pTPC_prong2 -> at(icand);                               else pTPC_prong2_ML=0;
+      if(t.pTOF_prong2) pTOF_prong2_ML=t.pTOF_prong2 -> at(icand);                               else pTOF_prong2_ML=0;
+      if(t.trlen_prong2) trlen_prong2_ML=t.trlen_prong2 -> at(icand);                            else trlen_prong2_ML=0;
+      if(t.start_time_res_prong2) start_time_res_prong2_ML=t.start_time_res_prong2 -> at(icand); else start_time_res_prong2_ML=0;
 
       fTreeLcML->Fill();
     }
@@ -317,13 +327,13 @@ void skimTreeLcFromEvt(TString input="AnalysisResults.root",TString output="test
 
 int main(int argc, char *argv[])
 {
-  if((argc != 6))
+  if((argc != 7))
   {
     std::cout << "Wrong number of inputs" << std::endl;
     return 1;
   }
   
-  if(argc == 6)
-    skimTreeLcFromEvt(argv[1],argv[2],argv[3],atoi(argv[4]),atoi(argv[5]));
+  if(argc == 7)
+    skimTreeLcFromEvt(argv[1],argv[2],argv[3],atoi(argv[4]),atoi(argv[5]),atoi(argv[6]));
   return 0;
 }
